29de272fc3f54ed89343e20dee7291fcbd853b34
Monika K S <manasaks545@gmail.com>
2026-07-21 11:21:34 +0530

    Added DNS resolution and port 80 scanning

--- port_scanner.py
+++ port_scanner.py
@@ -1,2 +1,25 @@
-print("Hello, GitHub!")
-print("Python Port Scanner Project")
+import socket
+
+print("=== Python Port Scanner ===")
+
+target = input("Enter IP address or domain: ")
+
+try:
+    ip = socket.gethostbyname(target)
+
+    print("Target:", target)
+    print("IP Address:", ip)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    result = s.connect_ex((ip, 80))
+
+    if result == 0:
+        print("Port 80 is OPEN")
+    else:
+        print("Port 80 is CLOSED")
+
+    s.close()
+
+except socket.gaierror:
+    print("Error: Unable to resolve the hostname.")
